a13d9cfac272fa26fdac96ba307711560037bb44
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-03-15 14:54:17 -0300

    Private IP for zookeeper and internal hostname for influxdb and grafana

--- start.sh
+++ start.sh
@@ -9,9 +9,9 @@ TMP_FILE="tmp.json"
 DASHBOARD_DIR="$PWD/.data/grafana/dashboards/"
 DASHBOARD_PATH=$DASHBOARD_DIR"cluster.json"
 INTERVAL=25
-GRAFANA_HOST="http://admin:admin@a12c20ec4090011e79d540a75cb326ba-851496104.us-east-1.elb.amazonaws.com:3000"
-INFLUXDB_HOST="http://root:root@100.68.195.141:8086"
-ZOOKEEPER_HOST="54.145.157.84:2181/kafka-rts"
+GRAFANA_HOST="http://admin:admin@grafana:3000"
+INFLUXDB_HOST="http://root:root@influxdb:8086"
+ZOOKEEPER_HOST="10.0.1.220:2181/kafka-rts"
 
 mkdir -p "$DASHBOARD_DIR"
 CONFIGS=$(./discovery/build/scripts/discovery --zookeeper "$ZOOKEEPER_HOST" --dashboard "cluster" --dashboard-path $DASHBOARD_DIR --interval "$INTERVAL" --influxdb "$INFLUXDB_HOST")
