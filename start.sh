7a66f4942b148b936e5bebf45ec1d12f801eb45d
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-03-16 11:32:42 -0300

    Automatically link influx to brokers

--- start.sh
+++ start.sh
@@ -6,21 +6,24 @@
 ###################################################################
 
 TMP_FILE="tmp.json"
-DASHBOARD_DIR="$PWD/.data/grafana/dashboards/"
-DASHBOARD_PATH=$DASHBOARD_DIR"cluster.json"
+CLUSTER_CONFIG="cluster.properties"
+DASHBOARD_DIR="$PWD/.data/grafana/dashboards"
+DASHBOARD_PATH="$DASHBOARD_DIR/cluster.json"
 INTERVAL=25
+#GRAFANA_HOST="$1"
+#INFLUXDB_HOST="$2"
 GRAFANA_HOST="http://admin:admin@grafana:3000"
 INFLUXDB_HOST="http://root:root@influxdb:8086"
-ZOOKEEPER_HOST="10.0.1.220:2181/kafka-rts"
+ZOOKEEPER_HOST="54.145.157.84:2181/kafka-rts"
 
 mkdir -p "$DASHBOARD_DIR"
-CONFIGS=$(./discovery/build/scripts/discovery --zookeeper "$ZOOKEEPER_HOST" --dashboard "cluster" --dashboard-path $DASHBOARD_DIR --interval "$INTERVAL" --influxdb "$INFLUXDB_HOST")
+./discovery/build/scripts/discovery --zookeeper "$ZOOKEEPER_HOST" --dashboard "cluster" --dashboard-path $DASHBOARD_DIR --interval "$INTERVAL" --influxdb "$INFLUXDB_HOST" > $CLUSTER_CONFIG
 
-echo '{"dashboard":'          >> $TMP_FILE 
-echo "$(cat $DASHBOARD_PATH)" >> $TMP_FILE
-echo ', "overwrite": true}'   >> $TMP_FILE
+echo '{"dashboard":'        >  $TMP_FILE
+cat $DASHBOARD_PATH         >> $TMP_FILE
+echo ', "overwrite": true}' >> $TMP_FILE
 
 echo -e "Loading dashboard\n$(curl -H "Content-Type: application/json" --data @$TMP_FILE "$GRAFANA_HOST/api/dashboards/db")\nDone"
 rm $TMP_FILE
 
-./influxdb-loader/build/scripts/influxdb-loader influxdb-loader/conf/local-jmx.properties "$CONFIGS"
+./influxdb-loader/build/scripts/influxdb-loader "$CLUSTER_CONFIG"
